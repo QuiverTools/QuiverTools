47dc37a3b51359280304714b60b93e3bc867d2bb
Pieter Belmans <pieterbelmans@gmail.com>
2024-06-06 13:43:50 +0200

    style: black

--- snippets/constructor.py
+++ snippets/constructor.py
@@ -1,11 +1,14 @@
 # constructor for a quiver from string à la msinvar
 
+
 def construct_from_strings(input):
-    arrows = [[int(v) for v in chain.split('-')] for chain in input] # list of lists of strings
+    arrows = [
+        [int(v) for v in chain.split("-")] for chain in input
+    ]  # list of lists of strings
     n = max(max(chain) for chain in arrows)
 
-    out = Matrix([[0]*n for i in range(n)])
+    out = Matrix([[0] * n for i in range(n)])
     for chain in arrows:
-        for i in range(len(chain)-1):
-            out[chain[i]-1, chain[i+1]-1] += 1
+        for i in range(len(chain) - 1):
+            out[chain[i] - 1, chain[i + 1] - 1] += 1
     return out
