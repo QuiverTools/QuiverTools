d3447aebb920e01a67a2eb5549b6de73cb3ff9de
Pieter Belmans <pieterbelmans@gmail.com>
2024-06-06 13:42:59 +0200

    style: apply black on some other files

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
 setuptools.setup(
-        name="quiver",
-        version="0.1",
-        description="Work with quivers, their representations, and their moduli spaces",
-        url="https://quiver.tools",
-        packages=setuptools.find_packages(),
-        zip_safe=False,
+    name="quiver",
+    version="0.1",
+    description="Work with quivers, their representations, and their moduli spaces",
+    url="https://quiver.tools",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
 )
-
