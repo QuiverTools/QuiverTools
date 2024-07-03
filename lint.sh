5d6ebfcf0fd8bdc7185543c0a97838d13b71c01c
Pieter Belmans <pieterbelmans@gmail.com>
2024-07-03 09:20:14 +0200

    chore: use extend-ignore in flake8
    This doesn't turn off the default ignores, so W503 remains ignored (as it is incompatible with black).

--- lint.sh
+++ lint.sh
@@ -7,7 +7,7 @@ echo "isort"
 isort --profile black quiver/*.py
 
 echo "flake8"
-flake8 --ignore=E741 --max-line-length 88 quiver/*.py
+flake8 --extend-ignore=E741 --max-line-length 88 quiver/*.py
 
 echo "ruff"
 ruff check --ignore=E741 quiver/*.py
