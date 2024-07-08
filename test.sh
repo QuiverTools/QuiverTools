7b9568926d4b92bafc0d46b0761f4a9732fde7cb
Pieter Belmans <pieterbelmans@gmail.com>
2024-07-08 09:36:59 +0200

    ci: run sage -t on directory not individual files

--- test.sh
+++ test.sh
@@ -1,13 +1,8 @@
-
 # doctests
-sage -t quiver/quiver.py
-sage -t quiver/constructions.py
-sage -t quiver/moduli.py
+sage -t quiver
 
 # coverage
-sage -coverage quiver/quiver.py
-sage -coverage quiver/constructions.py
-sage -coverage quiver/moduli.py
+sage -coverage quiver
 
 # sage -coverage.py quiver/constructions.py
 # sage -coverage.py quiver/quiver.py
