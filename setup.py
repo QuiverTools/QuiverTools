b56002bd48c0b2e7c0649f9e8790b168a2abd578
Pieter Belmans <pieterbelmans@gmail.com>
2025-06-19 09:50:18 +0200

    build: bump version
    Also, bump pre-commit Python version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="quiver",
-    version="1.0",
+    version="1.1",
     description="Work with quivers, their representations, and their moduli spaces",
     url="https://quiver.tools",
     packages=setuptools.find_packages(),
